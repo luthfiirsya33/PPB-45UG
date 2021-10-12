5e82c8d49fb4b6ceaa14425c2aaace9569fe3edc
Luthfi Irsyadurrafi <upiirsya33@gmail.com>
2021-10-12 22:53:53 +0700

    Update Pengumpulan Tugas 3

--- Kalkulator/app/src/main/java/com/example/kalkulator/MainActivity.java
+++ Kalkulator/app/src/main/java/com/example/kalkulator/MainActivity.java
@@ -12,6 +12,10 @@ public class MainActivity extends AppCompatActivity {
     EditText angk1,angk2;
     TextView hasilnya;
     Button tmbltambah;
+    Button tmblkurang;
+    Button tmblkali;
+    Button tmblbagi;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,7 +23,10 @@ public class MainActivity extends AppCompatActivity {
         angk1=(EditText) findViewById(R.id.angka1);
         angk2=(EditText) findViewById(R.id.angka2);
         hasilnya=(TextView) findViewById(R.id.hasilhitung);
-        tmbltambah=(Button)findViewById(R.id.tblHitung);
+        tmbltambah=(Button)findViewById(R.id.tblTambah);
+        tmblkurang=(Button)findViewById(R.id.tblKurang);
+        tmblkali=(Button)findViewById(R.id.tblKali);
+        tmblbagi=(Button)findViewById(R.id.tblBagi);
 
         tmbltambah.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -27,6 +34,33 @@ public class MainActivity extends AppCompatActivity {
                 int a1=Integer.parseInt(angk1.getText().toString());
                 int a2=Integer.parseInt(angk2.getText().toString());
                 Integer hasil=a1+a2;
+                hasilnya.setText(hasil.toString());
+            }
+        });
+        tmblkurang.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                int a1=Integer.parseInt(angk1.getText().toString());
+                int a2=Integer.parseInt(angk2.getText().toString());
+                Integer hasil=a1-a2;
+                hasilnya.setText(hasil.toString());
+            }
+        });
+        tmblkali.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                int a1=Integer.parseInt(angk1.getText().toString());
+                int a2=Integer.parseInt(angk2.getText().toString());
+                Integer hasil=a1*a2;
+                hasilnya.setText(hasil.toString());
+            }
+        });
+        tmblbagi.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                int a1=Integer.parseInt(angk1.getText().toString());
+                int a2=Integer.parseInt(angk2.getText().toString());
+                Integer hasil=a1/a2;
                 hasilnya.setText(hasil.toString());
             }
         });
