11e9e3a898a3f3d3bbda3bbccff36a1f58f06968
Luthfi Irsyadurrafi <upiirsya33@gmail.com>
2021-10-24 21:13:02 +0700

    Pengumpulan tugas SQLite anime

--- MyAnime/app/src/main/java/com/example/myanime/MainActivity.java
+++ MyAnime/app/src/main/java/com/example/myanime/MainActivity.java
@@ -8,8 +8,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.app.AlertDialog;
 import android.database.Cursor;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -130,26 +128,4 @@ public class MainActivity extends AppCompatActivity {
         builder.show();
     }
 
-
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.menu_main, menu);
-//        return true;
-//    }
-
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
-//
-//        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-//
-//        return super.onOptionsItemSelected(item);
-//    }
 }
